9c64b5b9585b0049cbca64b2d79d7aa685917782
james leviner <jamesleviner@jamess-MacBook-Pro.local>
2015-08-29 00:20:25 -0400

    usage

---
+++ app/src/main/java/com/example/miguele/superkids/activity/UStats.java
@@ -0,0 +1,85 @@
+package com.example.miguele.superkids.activity;
+
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+public class UStats {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("M-d-yyyy HH:mm:ss");
+    public static final String TAG = UStats.class.getSimpleName();
+
+    @SuppressWarnings("ResourceType")
+    public static void getStats(Context context) {
+        UsageStatsManager usm = (UsageStatsManager) context.getSystemService("usagestats");
+        int interval = UsageStatsManager.INTERVAL_YEARLY;
+        Calendar calendar = Calendar.getInstance();
+        long endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.YEAR, -1);
+        long startTime = calendar.getTimeInMillis();
+
+        Log.d(TAG, "Range start:" + dateFormat.format(startTime));
+        Log.d(TAG, "Range end:" + dateFormat.format(endTime));
+
+        UsageEvents uEvents = usm.queryEvents(startTime, endTime);
+        while (uEvents.hasNextEvent()) {
+            UsageEvents.Event e = new UsageEvents.Event();
+            uEvents.getNextEvent(e);
+
+            if (e != null) {
+                Log.d(TAG, "Event: " + e.getPackageName() + "\t" + e.getTimeStamp());
+            }
+        }
+    }
+
+    public static List<UsageStats> getUsageStatsList(Context context) {
+        UsageStatsManager usm = getUsageStatsManager(context);
+        Calendar calendar = Calendar.getInstance();
+        long endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.YEAR, -1);
+        long startTime = calendar.getTimeInMillis();
+
+        Log.d(TAG, "Range start:" + dateFormat.format(startTime));
+        Log.d(TAG, "Range end:" + dateFormat.format(endTime));
+
+        List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, startTime, endTime);
+        return usageStatsList;
+    }
+
+    public static void printUsageStats(List<UsageStats> usageStatsList) {
+
+        for (UsageStats u : usageStatsList) {
+            long time = u.getTotalTimeInForeground();
+            int sec  = (int)(time/ 1000) % 60 ;
+            int min  = (int)((time/ (1000*60)) % 60);
+            int hr   = (int)((time/ (1000*60*60)) % 24);
+
+            Log.d(TAG, "Pkg: " + u.getPackageName() + "\t" + "ForegroundTime: "
+                    + hr + "h " + min + "m " + sec + "s");
+        }
+    }
+
+    public static void toastUsageStats(List<UsageStats> usageStatsList, Context context) {
+        for (UsageStats u : usageStatsList) {
+            Toast.makeText(context, "Pkg: " + u.getPackageName() + "\t" + "ForegroundTime: "
+                    + dateFormat.format(u.getTotalTimeInForeground()), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void printCurrentUsageStatus(Context context) {
+        printUsageStats(getUsageStatsList(context));
+        toastUsageStats(getUsageStatsList(context), context);
+    }
+
+    @SuppressWarnings("ResourceType")
+    private static UsageStatsManager getUsageStatsManager(Context context) {
+        UsageStatsManager usm = (UsageStatsManager) context.getSystemService("usagestats");
+        return usm;
+    }
+}
--- app/src/main/java/com/example/miguele/superkids/activity/Usage.java
+++ app/src/main/java/com/example/miguele/superkids/activity/Usage.java
@@ -1,10 +1,15 @@
-package com.example.miguele.superkids;
+package com.example.miguele.superkids.activity;
 
 import android.app.Activity;
+import android.app.usage.UsageStats;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.miguele.superkids.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,14 +21,19 @@ public class Usage extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.usage_layout);
         ButterKnife.bind(this);
+
+        //Check if permission enabled
+        if (UStats.getUsageStatsList(this).isEmpty()) {
+            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+            startActivity(intent);
+        }
 
         usageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                UStats.printCurrentUsageStatus(Usage.this);
             }
         });
 
