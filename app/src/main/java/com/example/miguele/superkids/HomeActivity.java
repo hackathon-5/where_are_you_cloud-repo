b9e697c35cc3fc886b4f171ec1add1ae7970f55b
james leviner <jamesleviner@jamess-MacBook-Pro.local>
2015-08-28 22:08:26 -0400

    removed comment

--- app/src/main/java/com/example/miguele/superkids/HomeActivity.java
+++ app/src/main/java/com/example/miguele/superkids/HomeActivity.java
@@ -11,7 +11,6 @@ public class HomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Hello there
         setContentView(R.layout.activity_home);
     }
 
